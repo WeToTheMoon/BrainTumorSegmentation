60c1790661351121e06322243e88826ebad4ef09
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-08 17:42:44 -0400

    Feed learning rate to Lookahead optimizers

--- utils/optimizers.py
+++ utils/optimizers.py
@@ -2,9 +2,9 @@ from tensorflow_addons.optimizers import Lookahead, RectifiedAdam
 from keras.optimizers import Adam
 
 
-def LH_Adam():
-    return Lookahead(Adam())
+def LH_Adam(learning_rate: float):
+    return Lookahead(Adam(learning_rate))
 
 
-def Ranger():
-    return Lookahead(RectifiedAdam())
+def Ranger(learning_rate: float):
+    return Lookahead(RectifiedAdam(learning_rate))
