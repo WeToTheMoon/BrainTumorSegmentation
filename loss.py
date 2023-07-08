cc360fd32a727c06df530f97f3b35a9ae2cb6338
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-08 16:43:54 -0400

    Add additional loss methods

--- loss.py
+++ loss.py
@@ -1,15 +1,50 @@
 import tensorflow as tf
 from metric import dice_coef_multilabel
+from metric import dice_coef
 
 
 def CE(y_true, y_pred):
+    """
+    Gets the categorical cross entropy for the labels
+    :param y_true:
+    :param y_pred:
+    """
     return tf.keras.losses.CategoricalCrossentropy(y_true, y_pred)
 
 
 def dice_loss(y_true, y_pred):
     """
-    Takes the normal dice loss (Not weighted)
+    Gets the dice loss for the multiclass labels
     :param y_true:
     :param y_pred:
     """
     return 1 - dice_coef_multilabel(y_true, y_pred)
+
+
+def dice_coef_loss(y_true, y_pred):
+    """
+    Gets the dice loss for the binary labels
+    :param y_true:
+    :param y_pred:
+    """
+    return 1 - dice_coef(y_true, y_pred)
+
+
+def log_cosh_dice_loss(y_true, y_pred):
+    """
+    Gets the log cosh dice loss for the binary labels
+    :param y_true:
+    :param y_pred:
+    """
+    x = dice_loss(y_true, y_pred)
+    return tf.math.log((tf.exp(x) + tf.exp(-x)) / 2.0)
+
+
+def log_cosh_dice_loss_binary(y_true, y_pred):
+    """
+    Gets the log cosh dice loss for the binary labels
+    :param y_true:
+    :param y_pred:
+    """
+    x = dice_coef_loss(y_true, y_pred)
+    return tf.math.log((tf.exp(x) + tf.exp(-x)) / 2.0)
