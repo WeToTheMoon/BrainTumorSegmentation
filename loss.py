1c23906a178ba8c2af6242bb8d12a1ee62549d24
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2023-07-07 22:03:27 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,16 +1,15 @@
-from metric import dice_coef_multilabel, peritumoral_edema, core_tumor, enhancing_tumor
 import tensorflow as tf
+from metric import dice_coef_multilabel
+
 
 def CE(y_true, y_pred):
     return tf.keras.losses.CategoricalCrossentropy(y_true, y_pred)
 
+
 def dice_loss(y_true, y_pred):
-    '''
+    """
     Takes the normal dice loss (Not weighted)
     :param y_true:
     :param y_pred:
-    '''
-    return (1-dice_coef_multilabel(y_true, y_pred))
-
-
-
+    """
+    return 1 - dice_coef_multilabel(y_true, y_pred)
